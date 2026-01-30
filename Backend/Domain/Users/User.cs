d120baa9a8b7a1a81decf84061a0357c494ee943
Ruben Amaro <ruben95amaro@gmail.com>
2026-01-30 02:07:17 +0000

    Adding: Clean Architecture Folders
    Adding: SharedKernel, centralizes small, stable, framework-agnostic building blocks that are shared across layers, reducing duplication while preserving clear architectural boundaries.
    Adding: Domain, models the business rules and behavior of the system, remaining completely independent of frameworks and infrastructure.
    Adding: Application, coordinates domain objects to execute specific use cases, defining application behavior without depending on infrastructure or delivery mechanisms.
    Adding: Infrastructure, provides technical implementations for external concerns while remaining isolated from business rules and application logic.

---
+++ Backend/Domain/Users/User.cs
@@ -0,0 +1,21 @@
+﻿
+using Microsoft.AspNet.Identity.EntityFramework;
+
+using SharedKernel;
+
+namespace Domain.Users
+{
+    public sealed class User : IdentityUser
+    {
+        public Guid Id { get; set; }
+
+        public string? FirstName { get; set; }
+
+        public string? LastName { get; set; }
+
+        public string? Email { get; set; }
+
+        public String? CreateAt {  get; set; }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# LifeStack
+LifeStack is a smart life manager that merges tasks and money into one powerful flow, turning daily actions into clear financial awareness so you stay in control, avoid surprises, and make better decisions every day.
